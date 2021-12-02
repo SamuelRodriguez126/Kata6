9494934da8496d8eeeea2e75d6988ec7c981bfb7
SamuelRodri126 <samuel.rodriguez126@alu.ulpgc.es>
2021-12-02 16:18:58 +0000

    Kata6 Version 1

---
+++ src/toys/Car.java
@@ -0,0 +1,23 @@
+package toys;
+
+
+public class Car {
+
+    private final Integer serialNumber;
+    
+    public Car(Integer serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Integer getSerialNumber() {
+        return serialNumber;
+    }
+    
+    public void pack(){
+        System.out.println("Packing car: " + this.getSerialNumber());
+    }
+    
+    public void label(){
+        System.out.println("Labelling car: " + this.getSerialNumber());
+    }
+}
